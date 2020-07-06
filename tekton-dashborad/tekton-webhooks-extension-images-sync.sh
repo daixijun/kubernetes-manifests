0df4f587889a34ca4f6f562081f163fded294dbf
Xijun Dai <daixijun1990@gmail.com>
2020-07-06 13:14:37 +0800

    feat: update tekton dashboard webhook extension version to 0.7.0

--- tekton-dashborad/tekton-webhooks-extension-images-sync.sh
+++ tekton-dashborad/tekton-webhooks-extension-images-sync.sh
@@ -1,4 +1,4 @@
-VERSION=v0.5.1
+VERSION=v0.7.0
 ORG_REPO=gcr.io/tekton-releases/github.com/tektoncd/experimental/webhooks-extension/cmd/
 DST_REPO=registry.cn-hangzhou.aliyuncs.com/tekton-releases/webhooks-extension-
 IMAGES=(
